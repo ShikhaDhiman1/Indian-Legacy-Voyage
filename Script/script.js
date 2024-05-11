5a749affa933f469865da97500dcc6d14c21a529
Shikha Dhiman <dshikha4403@gmail.com>
2024-05-11 18:53:12 +0530

    changed path

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,26 @@
 const divForm = document.getElementById("form-div");
 
+function validate_password(){
+  var passw = document.getElementById("passw").value;
+var confirmPassw = document.getElementById("confirmPassword").value;
+
+if(passw != confirmPassw){
+  document.getElementById("wrong_pass_alert").style.color = "red";
+  document.getElementById("wrong_pass_alert").innerHTML = 'Use correct password!';
+  document.getElementById('formSubmit').disabled = true;
+  document.getElementById('formSubmit').style.opacity = (0.4);
+  document.getElementById('formSubmit').style.cursor = "not-allowed"
+}
+else{
+  document.getElementById("wrong_pass_alert").innerHTML = 'Correct password!';
+  document.getElementById('formSubmit').disabled = false;
+  document.getElementById('formSubmit').style.opacity = (1);
+  document.getElementById('formSubmit').style.cursor = 'pointer';
+}
+}
+
+
+
 function formAction() {
   const bodyDiv = document.getElementById("body-cover");
   bodyDiv.classList.remove("body-div");
@@ -24,7 +45,7 @@ function clickSignIn() {
 
 function clickSignUp() {
   divForm.innerHTML =
-    "<center><h3 class='form-heading'>SIGN UP</h3></center> <form action='#' onsubmit='formAction()'> <label for='Fname'>First Name:<span class='sup'> *</span></label> <input type='text' name='fname' id='Fname' required placeholder='Your first name' /><br /> <label for='Lname'>Last Name:</label> <input type='text' name='lname' id='Lname' placeholder='Your last name' /><br /> <label for='email'>E-mail Id:<span class='sup'> *</span></label> <input type='email' name='email' id='email' required placeholder='E-mail'  /><br /> <label for='passw'>Password:<span class='sup'> *</span></label> <input type='password' name='password' id='passw' required placeholder='password' /><br /> <label for='confirmPassword'>Confirm Password:<span class='sup'> *</span></label> <input type='password' name='confpass' id='confirmPassword' required placeholder='password' /><br /> <input id='formSubmit_register' type='submit' value='SUBMIT'> <hr> <p class='sign-up'>Already a have an account? <a onclick = 'clickSignIn()' id = 'link-login' href='#'>SIGN IN</a> </p> </form>";
+    "<center><h3 class='form-heading'>SIGN UP</h3></center> <form action='#' onsubmit='formAction()'> <label for='Fname'>First Name:<span class='sup'> *</span></label> <input type='text' name='fname' id='Fname' required placeholder='Your first name' /><br /> <label for='Lname'>Last Name:</label> <input type='text' name='lname' id='Lname' placeholder='Your last name' /><br /> <label for='email'>E-mail Id:<span class='sup'> *</span></label> <input type='email' name='email' id='email' required placeholder='E-mail'  /><br /> <label for='passw'>Password:<span class='sup'> *</span></label> <input type='password' name='password' id='passw' required placeholder='password' /><br /> <label for='confirmPassword'>Confirm Password:<span class='sup'> *</span></label> <input type='password' name='confpass' id='confirmPassword' required placeholder='password' onkeyup='validate_password()' /><br /> <input id='formSubmit_register' type='submit' value='SUBMIT'> <hr> <p class='sign-up'>Already a have an account? <a onclick = 'clickSignIn()' id = 'link-login' href='#'>SIGN IN</a> </p> </form>";
 }
 
 function clickUserLogin() {
